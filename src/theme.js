9a89f3d02a8800371bc835798519792fbdd10f24
valtermro <valtermro@outlook.com>
2021-06-12 17:01:31 -0300

    Boilerplate

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontSize: 28
+    },
+    h2: {
+      fontSize: 24
+    },
+    h3: {
+      fontSize: 18
+    }
+  }
+});
+
+export default theme;
